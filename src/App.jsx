ebf32111f313a2501dc2a9e9002cf510495a9f9a
Diogo Oliveira <112041509+Dioguinxz@users.noreply.github.com>
2023-11-16 22:15:59 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import { Card } from './Card/Card.jsx';
+
+
+
+function App() {
+    const [count, setCount] = useState(0)
+
+    return (
+        <>
+            <Card />
+            <Card />
+
+        </>
+    )
+}
+
+export default App
